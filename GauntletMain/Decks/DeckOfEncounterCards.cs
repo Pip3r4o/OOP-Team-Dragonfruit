f6ec5eaf27cd4418220731cb7b9c8c04fad4c82e
elfoles <stefan.mirevski@gmail.com>
2015-03-27 23:27:14 +0200

    Adding the actual file DeckOfEncounterCards.cs

---
+++ GauntletMain/Decks/DeckOfEncounterCards.cs
@@ -0,0 +1,18 @@
+﻿using GauntletMain.Classes;
+
+namespace GauntletMain.Decks
+{
+    class DeckOfEncounterCards : Deck<Encounter>
+    {
+        #region Constructors
+        public DeckOfEncounterCards()
+        {
+            DeckOfMonsterCards deckOfMonsterCards = new DeckOfMonsterCards();
+            DeckOfModifierCards deckOfModifierCards = new DeckOfModifierCards();
+
+            listOfCards.AddRange(deckOfMonsterCards.ListOfCards);
+            listOfCards.AddRange(deckOfModifierCards.ListOfCards);
+        }
+        #endregion
+    }
+}
